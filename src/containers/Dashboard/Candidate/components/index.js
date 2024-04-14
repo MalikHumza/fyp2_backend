4c1a6f2a3a85721d07958f36e84c8cb84275ee89
MalikHumza <humzamalik1146@gmail.com>
2024-04-14 21:02:49 +0500

    feat: project-setup

---
+++ src/containers/Dashboard/Candidate/components/index.js
@@ -0,0 +1,18 @@
+import CandidateSidebar from "./CandidateSidebar";
+import CandidateDashboard from "./CandidateDashboard"
+import Resume from "./Resume"
+import Favourite from "./Favourite"
+import EditProfile from "./EditProfile"
+import AppliedJobs from "./AppliedJobs"
+import FindAJob from "./FindAJob";
+
+export{
+    CandidateDashboard,
+    CandidateSidebar,
+    Resume,
+    Favourite,
+    EditProfile,
+    AppliedJobs,
+    FindAJob
+
+}
